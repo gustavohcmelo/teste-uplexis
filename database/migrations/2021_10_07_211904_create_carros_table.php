89d1f098042d6ec5d28fa298d41f48bb0d67ecb9
Gustavo Melo <gustavo.ghcmelo@gmail.com>
2021-10-07 18:37:32 -0300

    tabela modelo e controle de Carros

---
+++ database/migrations/2021_10_07_211904_create_carros_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->references('id')->on('users');
+            $table->string('nome_veiculo');
+            $table->string('link');
+            $table->string('ano');
+            $table->string('combustivel');
+            $table->string('portas');
+            $table->integer('quilometragem');
+            $table->string('cambio');
+            $table->string('cor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carros');
+    }
+}
